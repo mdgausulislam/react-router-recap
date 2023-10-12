7bf35e70445f141f74c699e510052fe98be40fed
unknown <gausulislam5@gmail.com>
2023-10-12 17:24:01 +0600

    update linechart

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import DashBoard from '../DashBoard/DashBoard';
 
 const About = () => {
     return (
         <div>
             <h1>This is a About page</h1>
+            <DashBoard></DashBoard>
         </div>
     );
 };
