7944acf26c687f90384527d8f4cd98874fec422c
unknown <gausulislam5@gmail.com>
2023-10-12 17:08:11 +0600

    update active link header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/image/profile.png';
 import './Header.css';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
         <nav>
             <h1>Header</h1>
             <div className='menubar'>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/shop'>Shop</Link>
-                <Link to='/inventory'>Inventory</Link>
-                <Link to='/login'>Login</Link>
+                <ActiveLink to='/'>Home</ActiveLink>
+                <ActiveLink to='/about'>About</ActiveLink>
+                <ActiveLink to='/shop'>Shop</ActiveLink>
+                <ActiveLink to='/inventory'>Inventory</ActiveLink>
+                <ActiveLink to='/login'>Login</ActiveLink>
                 <img src={logo} alt="" />
             </div>
 
