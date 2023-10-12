355385699bcb155bcbb6816207fd3e055a100868
unknown <gausulislam5@gmail.com>
2023-10-12 12:47:54 +0600

    update header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/image/profile.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav>
+                <h1>Header</h1>
+            <div className='menubar'>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/shop'>Shop</Link>
+                <Link to='/inventory'>Inventory</Link>
+                <Link to='/login'>Login</Link>
+                <img src={logo} alt="" />
+            </div>
+
+        </nav>
+    );
+};
+
+export default Header;
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from './Header/Header';
 import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
 
 const Main = () => {
     return (
         <div>
-            <Header></Header>
+           <Header></Header>
             <Outlet></Outlet>
         </div>
     );
